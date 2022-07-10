c849a67bf9297fa4401d2bde69e314103ab5bc0b
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-07-10 19:37:12 +1000

    onclick function added to button, coordinates function corrected

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
-const APIKey = 'b736d3ccba7d12a6bc0de9a4c8ad9005'
-const searchButton = document.getElementById("#search-btn")
+const APIKey = '97dbde334bd059edbd3fa8fbd7489001';
+const searchButton = document.getElementById("#search-btn");
+const chosenCity = document.querySelector('#cityname');
+let currentCity = ""
 
-let chosenCity = 'Sydney'
-
-searchButton.addEventListener("click", function(getCityName){
-    getCityName.preventDefault()
-    var userInputEL = document.getElementById("#cityname").value
-    console.log(userInputEL)
-    userInput(userInputEL)
- } );
+function cityName() {
+    currentCity = chosenCity.value;
+    getCoordinates(currentCity);
+    chosenCity.value = "";
+    console.log(currentCity)
+}
 
 // Get users input 
 
-function userInput(userInputEL){
-    chosenCity = userInputEL;
-    getCoordinates(chosenCity)
-}
-
 // Get searched for city lat and long 
 
-function getCoordinates(chosenCity) { 
+function getCoordinates(currentCity) { 
     fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=" 
-    + chosenCity 
-    + "&appid=b736d3ccba7d12a6bc0de9a4c8ad9005")
+    + currentCity 
+    + "&appid=97dbde334bd059edbd3fa8fbd7489001")
     .then((response) => response.json())
     .then(function (data){
         lat = data.coord.lat;
         long = data.coord.lon;
+        console.log(lat, long)
     })
 
 }
