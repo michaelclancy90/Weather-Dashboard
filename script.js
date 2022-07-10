5af0127134a15740712ce14d8a4455ce3fd50a6c
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-07-10 16:28:52 +1000

    user input function added, function added to get coordinates

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-var APIKey = 'b736d3ccba7d12a6bc0de9a4c8ad9005'
+const APIKey = 'b736d3ccba7d12a6bc0de9a4c8ad9005'
+const searchButton = document.getElementById("#search-btn")
+
+let chosenCity = 'Sydney'
+
+searchButton.addEventListener("click", function(getCityName){
+    getCityName.preventDefault()
+    var userInputEL = document.getElementById("#cityname").value
+    console.log(userInputEL)
+    userInput(userInputEL)
+ } );
+
+// Get users input 
+
+function userInput(userInputEL){
+    chosenCity = userInputEL;
+    getCoordinates(chosenCity)
+}
+
+// Get searched for city lat and long 
+
+function getCoordinates(chosenCity) { 
+    fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" 
+    + chosenCity 
+    + "&appid=b736d3ccba7d12a6bc0de9a4c8ad9005")
+    .then((response) => response.json())
+    .then(function (data){
+        lat = data.coord.lat;
+        long = data.coord.lon;
+    })
+
+}
+
+
+
+
